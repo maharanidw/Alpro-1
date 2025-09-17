b07f0200f7f5a4c81411c411c48b2dd4bcee5490
Maharani Dewi Wulansari <115380672+maharanidw@users.noreply.github.com>
2025-09-17 20:06:42 +0700

    First commit

---
+++ BiodataTeman.java
@@ -0,0 +1,13 @@
+package praktikumalpro;
+public class BiodataTeman {
+     public static void main(String[] args) {
+        System.out.println("-----BIODATA TEMANKU-----");
+        System.out.println("Nama Lengkap        : Laudiya Meysinta Dewi");
+        System.out.println("Nama Panggilan      : Laudi");
+        System.out.println("NRP                 : 5002241123");
+        System.out.println("Angkatan            : 2024");
+        System.out.println("Asal                : Banyuwangi");
+        System.out.println("TTL                 : Banyuwangi, 5 Mei 2007");
+        System.out.println("Dosen Wali          : Pak Isa Irawan");
+     }
+}
